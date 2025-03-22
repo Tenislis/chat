856fd5b6edaed6f6f06b2333962ce493d053698e
Denis <deniskingsm@gmail>
2025-03-22 16:14:09 +0300

    aster

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
+export default{
   compatibilityDate: '2024-11-01',
+  target: 'static',
+  generate:{
+    fallback: false
+  },
   devtools: { enabled: true },
   modules:['@nuxt/ui'],
   nitro:{
@@ -8,4 +12,4 @@ export default defineNuxtConfig({
         websocket: true
     }
   }
-})
+}
